f417bb0cde87102c778cdbefd72bb9bce2114620
Majid Abdallah <majidabdallah566@gmail.com>
2026-02-12 07:36:04 -0800

    commited file

--- app.py
+++ app.py
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-import streamlit as st
-import joblib
-import numpy as np
-
-# Load model
-model = joblib.load("model.pkl")
-
-st.title("🎓 Student Performance Prediction App")
-st.write("Enter student details to predict final score.")
-
-# User inputs
-study_hours = st.number_input("Study Hours per Day", min_value=1, max_value=12, value=5)
-attendance = st.number_input("Attendance Percentage", min_value=50, max_value=100, value=80)
-previous_score = st.number_input("Previous Exam Score", min_value=0, max_value=100, value=65)
-
-# Prediction
-if st.button("Predict Final Score"):
-    input_data = np.array([[study_hours, attendance, previous_score]])
-    prediction = model.predict(input_data)
-    st.success(f"📊 Predicted Final Score: {prediction[0]:.2f}")
-=======
 import streamlit as st
 import joblib
 import numpy as np
@@ -37,4 +15,3 @@ if st.button("Predict Final Score"):
     input_data = np.array([[study_hours, attendance, previous_score]])
     prediction = model.predict(input_data)
     st.success(f"📊 Predicted Final Score: {prediction[0]:.2f}")
->>>>>>> c183d60 (Initial commit with project files)
